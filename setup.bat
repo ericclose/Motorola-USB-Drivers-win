6d9e7251a213be80150bcd8621550113b0cc6c1a
ericclose <ZetaGo@protonmail.com>
2023-04-05 08:12:04 +0800

    use double quotes prevent the path with space

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 @echo off
 
-pnputil /add-driver %~dp0\Motorola\*.inf /subdirs /install
-pnputil /add-driver %~dp0\usb_driver\*.inf /subdirs /install
+pnputil /add-driver "%~dp0Motorola\*.inf" /subdirs /install
+pnputil /add-driver "%~dp0usb_driver\*.inf" /subdirs /install
 
 PAUSE
